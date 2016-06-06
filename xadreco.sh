388bf3d3ef018178ca20e17148567654f82c5ce3
Ruben Carlo Benante <rcb@beco.cc>
2016-06-06 17:38:37 -0300

    xadreco.sh script to call it from Xboard

---
+++ xadreco.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#***************************************************************************
+#*   xadreco.sh                    Build 20160606.173220, see VERSION file *
+#*                                                                         *
+#*   Xadreco chess engine                                                  *
+#*   Copyright (C) 1999-2016 by Ruben Carlo Benante <rcb@beco.cc>          *
+#***************************************************************************
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU General Public License as published by  *
+#*   the Free Software Foundation version 2 of the License.                *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU General Public License for more details.                          *
+#*                                                                         *
+#*   You should have received a copy of the GNU General Public License     *
+#*   along with this program; if not, write to the                         *
+#*   Free Software Foundation, Inc.,                                       *
+#*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+#***************************************************************************
+#*   To contact the author, please write to:                               *
+#*   Ruben Carlo Benante                                                   *
+#*   Email: rcb@beco.cc                                                    *
+#*   Webpage: http://xadreco.beco.cc/                                      *
+#***************************************************************************
+
+xboard -tc 1 -inc 0 -mps 40 -size petite -coords -highlight -fd "." -fcp "./xadreco" -sd "." -scp "./xadreco"
+
+#end of xadreco.sh file
+
